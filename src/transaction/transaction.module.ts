f9942be940cbbaddcf15fa6fee64b0e3d992b777
Belriax <gleiconsousa@gmail.com>
2024-12-19 16:16:39 -0300

    Atualização de refatoração

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,4 +1,3 @@
-// src/transaction/transaction.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
