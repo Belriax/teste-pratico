9845e35d38fac9abeb8e7541d74d071a34076085
Gleicon Sousa <43761879+Belriax@users.noreply.github.com>
2024-12-29 12:40:38 -0300

    Update postgres.config.service.ts

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -9,7 +9,7 @@ export class PostgresConfigServices implements TypeOrmOptionsFactory {
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT) || 5432,
       username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'Bemafig32##',
+      password: process.env.DB_PASSWORD || '#######',
       database: process.env.DB_NAME || 'testedb',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
